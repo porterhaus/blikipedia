7ab0acd16fad08570a32069aa425d285f10b39b5
porterhaus <bporter@porterhaus.com>
2015-05-10 16:10:04 -0500

    Refactoring wiki CRUD.

--- app/views/layouts/readings.rb
+++
@@ -1,8 +0,0 @@
-      t.decimal :meter_value
-      t.dateime :time_of_test
-      t.text    :comments
-      t.string  :activity
-      t.integer :carbohydrates
-      t.integer :basal_insulin_taken
-      t.integer :bolus_insulin_taken
-      t.timestamps null: false
